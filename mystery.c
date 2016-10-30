4db82cd7558f5198b00d08cafbbc68e87bec7934
David Parsons <david.parsons@rutgers.edu>
2016-10-30 12:57:43 -0400

    n/a

--- mystery.c
+++ mystery.c
@@ -1,3 +1,20 @@
 int add (){
   return 0;
 }
+
+int atoi(char * input){
+  // stores int value
+  unsigned int val = 0;
+  // sums each int based of position
+  int pos = 0;
+  for(pos = 0; pos < strlen(input); pos++){
+   val *= 10;
+   val += input[pos] - '0';
+  }
+  return val;
+}
+
+int main(int argc, char **argv){
+
+  return 0;
+}
