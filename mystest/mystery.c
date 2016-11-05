202c000286f625155b7221b832e1d71679f57597
David Parsons <david.parsons@rutgers.edu>
2016-11-05 19:28:51 -0400

    Finished Mystery.c i.e. made it from assembly

--- mystest/mystery.c
+++ mystest/mystery.c
@@ -3,32 +3,72 @@
 #include <stdlib.h>
 
 
-int num[200];
+  int num[200]; // line 114 of mystery.c 32/4 = 4 -> 800/4 = 200
 
-int add(int x, int y){
-  return x + y;
-}
+  /*
+   * lines 3:12 of mystery.s
+   * Pushes base pointer copies stack pointer
+   * puts second parameter into return pointer
+   * sums first parameter and return pointer (i.e. sum of first and second parameters)
+   * pops base pointer, returns return pointer (eax)
+   */
+  int add(int x, int y){
+    return x + y;
+  }
 
-int fibb(int input){
-  // 7 local variables
-  int output;
-  if(num[input] == -1){
-    if(input == 1 || input == 0){
-      return input;
-    } else {
-      return fibb(input - 1) + fibb(input - 2);
+  /*
+   * lines 13:71 of mystery.c
+   * an optimized fibonnaci calculator
+   */
+
+  int fibb(int input){
+    /*
+     * 16:19 Pushes base pointer copies stack pointer and puts second parameter into return pointer
+     */
+    int output; //line 19, space allocated for local variables
+    /*
+     * lines 20:23
+     * compares the paramter poisition of the array to -1
+     * basically testing if the paramters fibonnaci value has been calculated
+     * jumps to .L4 or directly to output stage if -1 is equal to the "calculated fib number"
+     */
+    if(num[input] == -1){
+      int local1 = -1; // line 29
+      if(0 == input){ // line 30
+        local1 = 0; // line 32
+      } else if (1 == input) { // line 35
+        local1 = 1; // line 37
+      } else { // .L10
+        // lines 40:52
+        local1 = add(fibb(input - 2), fibb(input - 1));
+      }
+
+      // .L9 this is really redundant and it appears .L9 does this so...
+      if (num[input] == -1){ // lines 58:60
+        num[input] = local1;
+      }
+
+      // .L12
+      output = num[input];
+
+    } else { // lines 24:26
+
+      output = num[input];
+
     }
+    return output;
   }
-  output = num[input];
-  return output;
-}
 
-int main(int argc, char ** argv){
-  int i = 0;
-  while(i < 200){
-    num[i] = -1;
-    i++;
+  int main(int argc, char ** argv){
+    int input = atoi(argv[1]); // lines 89:91
+    int i = 0; // line 90
+    while(i < 199){ // line 99
+      /*
+       * .L17
+       */
+      num[i] = -1; // lines 95 and 96
+      i++; // line 97
+    }
+    printf("%d\n", fibb(input)); // lines 101:106
+    return 0;
   }
-  printf("%d\n", fibb(atoi(argv[1])));
-  return 1;
-}
